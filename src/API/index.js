d6921dbb42433f3bb2ecc5ccc66dea7c2d775278
jacksonk-dev <jacksonk.dev@gmail.com>
2022-01-13 17:51:26 +0300

    feat($App): fetch with axios
    Replaces fetch api with axios

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,6 @@
-const conversionURL = new URL('https://pro-api.coinmarketcap.com/v1/tools/price-conversion');
+import axios from 'axios';
+
+const conversionURL = 'https://pro-api.coinmarketcap.com/v1/tools/price-conversion';
 
 export const supportedCryptoCurrencies = [
   { BTC: 'Bitcoin' },
@@ -9,11 +11,16 @@ export const supportedFIATCurrencies = [
 ];
 
 export const convert = (amount = 5, from = 'BTC', to = 'USD') => {
-  conversionURL.searchParams.set('amount', amount);
-  conversionURL.searchParams.set('symbol', from);
-  conversionURL.searchParams.set('convert', to);
-
-  fetch(conversionURL)
+  axios.get(conversionURL, {
+    params: {
+      amount,
+      symbol: from,
+      convert: to,
+    },
+    headers: {
+      'X-CMC_PRO_API_KEY': '6c722efe-2833-4ffd-8a1c-f31b4113bf81',
+    },
+  })
     .then(response => response.json())
     .then(data => console.log(data));
 };
